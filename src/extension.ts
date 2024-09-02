d2c25f82a4e44764a1985bbdb5b718d72b894092
Tzipi-Winefeld <157100435+Tzipi-Winefeld@users.noreply.github.com>
2024-09-03 00:46:56 +0300

    The code page of the plugin

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.removeAllDebuggers', async () => {
+		// Gets all the open files in the project
+        const files = await vscode.workspace.findFiles('**/*.{js,ts}', '**/node_modules/**');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "debugger-wipe" is now active!');
+        for (const file of files) {
+            const document = await vscode.workspace.openTextDocument(file);
+            const edit = new vscode.WorkspaceEdit();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('debugger-wipe.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Debugger Wipe!');
-	});
+            for (let i = 0; i < document.lineCount; i++) {
+                const line = document.lineAt(i);
+                // Checking whether the line contains the word 'debugger'
+                if (line.text.includes('debugger')) {
+                    edit.delete(file, line.range);
+                }
+            }
 
-	context.subscriptions.push(disposable);
+            await vscode.workspace.applyEdit(edit);
+            await document.save();
+        }
+
+        vscode.window.showInformationMessage('All debugger statements have been removed!');
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
