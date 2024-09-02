e06185b5dc560cc39a5b66919182884aa8506232
Tzipi-Winefeld <157100435+Tzipi-Winefeld@users.noreply.github.com>
2024-09-03 02:01:34 +0300

    An extension of the plugin for the debugger note

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,64 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.removeAllDebuggers', async () => {
-		// Gets all the open files in the project
-        const files = await vscode.workspace.findFiles('**/*.{js,ts}', '**/node_modules/**');
+  let disposable = vscode.commands.registerCommand(
+    "extension.removeAllDebuggers",
+    async () => {
+      // Gets all the open files in the project
+      const files = await vscode.workspace.findFiles(
+        "**/*.{js,ts}",
+        "**/node_modules/**"
+      );
 
-        for (const file of files) {
-            const document = await vscode.workspace.openTextDocument(file);
-            const edit = new vscode.WorkspaceEdit();
+      for (const file of files) {
+        const document = await vscode.workspace.openTextDocument(file);
+        const edit = new vscode.WorkspaceEdit();
 
-            for (let i = 0; i < document.lineCount; i++) {
-                const line = document.lineAt(i);
-                // Checking whether the line contains the word 'debugger'
-                if (line.text.includes('debugger')) {
-                    edit.delete(file, line.range);
-                }
-            }
-
-            await vscode.workspace.applyEdit(edit);
-            await document.save();
+        for (let i = 0; i < document.lineCount; i++) {
+          const line = document.lineAt(i);
+          // Checking whether the line contains the word 'debugger'
+          if (line.text.includes("debugger")) {
+            edit.delete(file, line.range);
+          }
         }
 
-        vscode.window.showInformationMessage('All debugger statements have been removed!');
-    });
+        await vscode.workspace.applyEdit(edit);
+        await document.save();
+      }
 
-    context.subscriptions.push(disposable);
+      vscode.window.showInformationMessage(
+        "All debugger statements have been removed!"
+      );
+    }
+  );
+
+  // To pass the debugger commands to a comment
+  let commentDebuggersCommand = vscode.commands.registerCommand('extension.commentAllDebuggers', async () => {
+    const files = await vscode.workspace.findFiles('**/*.{js,ts}', '**/node_modules/**');
+
+    for (const file of files) {
+        const document = await vscode.workspace.openTextDocument(file);
+        const edit = new vscode.WorkspaceEdit();
+
+        for (let i = 0; i < document.lineCount; i++) {
+            const line = document.lineAt(i);
+            if (line.text.includes('debugger')) {
+                const startIdx = line.text.indexOf('debugger');
+                const range = new vscode.Range(i, startIdx, i, startIdx + 'debugger'.length);
+                edit.replace(file, range, `// debugger`);
+            }
+        }
+
+        await vscode.workspace.applyEdit(edit);
+        await document.save();
+    }
+
+    vscode.window.showInformationMessage('All debugger statements have been commented out!');
+});
+
+
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(commentDebuggersCommand);
 }
 
 export function deactivate() {}
