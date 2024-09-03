22c21cdc5224bd475c66107713b86dd6a41f54a5
Tzipi-Winefeld <157100435+Tzipi-Winefeld@users.noreply.github.com>
2024-09-03 18:28:45 +0300

    Deleting the word alone and deleting breakpoints

--- src/extension.ts
+++ src/extension.ts
@@ -4,33 +4,39 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "extension.removeAllDebuggers",
     async () => {
-      // Gets all the open files in the project
+      // Get all open files in the project
       const files = await vscode.workspace.findFiles(
         "**/*.{js,ts}",
         "**/node_modules/**"
       );
-
+  
       for (const file of files) {
         const document = await vscode.workspace.openTextDocument(file);
         const edit = new vscode.WorkspaceEdit();
-
+  
         for (let i = 0; i < document.lineCount; i++) {
           const line = document.lineAt(i);
-          // Checking whether the line contains the word 'debugger'
-          if (line.text.includes("debugger")) {
-            edit.delete(file, line.range);
+          // Check if the line contains the word 'debugger'
+          const debuggerIndex = line.text.indexOf("debugger");
+          if (debuggerIndex !== -1) {
+            const range = new vscode.Range(i, debuggerIndex, i, debuggerIndex + "debugger".length);
+            edit.delete(file, range);
           }
         }
-
+  
         await vscode.workspace.applyEdit(edit);
         await document.save();
       }
-
+  
+      // Remove all breakpoints
+      vscode.debug.removeBreakpoints(vscode.debug.breakpoints);
+  
       vscode.window.showInformationMessage(
-        "All debugger statements have been removed!"
+        "All debugger statements and breakpoints have been removed!"
       );
     }
   );
+  
 
   // To pass the debugger commands to a comment
   let commentDebuggersCommand = vscode.commands.registerCommand(
